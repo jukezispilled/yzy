1488b2c91cc822b33a3d12087627ae3efd865b6e
agorapilled <johngardner13@outlook.com>
2025-03-16 14:43:00 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TextScrambler from './scramble';
 // Main App Component
 function App() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-white font-custom">
+    <div className="h-[100dvh] w-screen flex justify-center items-center bg-white font-custom">
       <div className="grid content-center justify-items-center">
         <div className="absolute top-[10%] md:top-[25%] left-1/2 transform -translate-x-1/2 text-5xl md:text-6xl whitespace-nowrap">
           "ANTI-MONEY"
