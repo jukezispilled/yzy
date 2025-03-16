986660c729f8c0bcc10fbef6ba98e25f4295d67b
agorapilled <johngardner13@outlook.com>
2025-03-16 14:44:29 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="h-[100dvh] w-screen flex justify-center items-center bg-white font-custom">
       <div className="grid content-center justify-items-center">
-        <div className="absolute top-[10%] md:top-[25%] left-1/2 transform -translate-x-1/2 text-5xl md:text-6xl whitespace-nowrap">
+        <div className="absolute top-[5%] md:top-[25%] left-1/2 transform -translate-x-1/2 text-5xl md:text-6xl whitespace-nowrap">
           "ANTI-MONEY"
         </div>
         <TextScrambler />
